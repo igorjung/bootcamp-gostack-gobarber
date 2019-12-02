85ca58ea1b7a2860b93f036827f38e7a3188d519
igorjung <igor.jung@outlook.com>
2019-12-02 12:15:11 -0300

    Criando versão mobile

---
+++ mobile/src/pages/SignIn/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import Background from '~/components/Background';
+import Button from '~/components/Button';
+import Input from '~/components/Input';
+
+// import { Container } from './styles';
+
+export default function SignIn() {
+  return (
+    <Background>
+      <Text>SignIn</Text>
+
+      <Input style={{ marginTop: 30 }} icon="call" placeholder="Digite senha" />
+
+      <Button>Entrar</Button>
+    </Background>
+  );
+}
